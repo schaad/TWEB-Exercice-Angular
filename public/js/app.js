69effe0eec8b5517d38a557d854d781e26830330
Valentin Schaad <valentin.schaad@heig-vd.ch>
2015-11-12 16:41:18 +0100

    Ajout d'un chart

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,8 @@
-angular.module('scopeExample', [])
+angular.module('scopeExample', ['chart.js'])
 
 .controller('firstController', ['$scope', function($scope){
 	$scope.name = "Valentin";
-}])
+
+	$scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+  	$scope.data = [300, 500, 100];
+}]);
