e99dbc83a342f4564031ee17c2bb26c5ff6ca777
Valentin Schaad <valentin.schaad@heig-vd.ch>
2015-11-13 16:57:51 +0100

    (\ /) (O.o) (> <) Bunny approves these changes.

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,42 @@
-angular.module('scopeExample', ['chart.js'])
+var tweb = angular.module('tweb', ['ngRoute', 'chart.js']);
 
-.controller('firstController', ['$scope', function($scope){
-	$scope.name = "Valentin";
+tweb.controller('mainController', ['$route', '$routeParams', '$location',
+  function($route, $routeParams, $location) {
+    this.$route = $route;
+    this.$location = $location;
+    this.$routeParams = $routeParams;
+}])
+
+tweb.controller('firstController', ['$scope', function($scope){
 
 	$scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
   	$scope.data = [300, 500, 100];
+
+  	$scope.home = "active";
+  	$scope.admin = "";
+
+  	$scope.title = "Vos résultats";
 }]);
+
+
+tweb.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/home', {
+				templateUrl: 'home.html',
+				controller: 'firstController'
+			}).
+			when('/admin', {
+				templateUrl: 'admin.html',
+				controller: 'secondController'
+			}).
+			otherwise({
+				templateUrl: 'home.html',
+				controller: 'firstController'
+			});
+	}]);
+
+tweb.controller('secondController', function($scope){
+	$scope.home = "";
+  	$scope.admin = "active";
+});
