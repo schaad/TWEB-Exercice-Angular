65cf6dbaa2fc60caaccef8f1d756f12233e8e087
Valentin Schaad <valentin.schaad@heig-vd.ch>
2015-11-19 16:01:20 +0100

    Ajout de la troisième page, mise en place d'un semblant de communication.

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-
+// SERVEUR
+// Auteur : Valentin Schaad
 
 var express = require('express'),
-  config = require('./config/config');
+	config = require('./config/config');
 
 var app = express();
 
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var data = [0, 5, 0];
+var label = ["Super", "Just to have 3 colors", "Cool"]
+
 require('./config/express')(app, config);
 
-app.listen(config.port, function () {
+server.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
 
+io.on('connection', function(socket){
+	console.log('Un client s\'est connecté');
+
+	// Définition des événements de communication avec le client
+
+	socket.on('requestInit', function(){
+		socket.emit('initVote', data, label);
+	});
+
+	socket.on('voteFor', function(id){
+		++data[id];
+
+		console.log('Un update ' + data[id]);
+
+		io.emit('updateVote', data);
+	});
+
+	socket.on('reset', function(){
+		data = [0, 5, 0];
+
+		io.emit('updateVote', data);
+	});
+});
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,28 @@
+// CLIENT
+// Auteur : Valentin Schaad
+
+// Dégueulasse de mettre le port en dur...
+var socket = io.connect("127.0.0.1:3000");
+// Encore plus crade d'utiliser des variables globales, mais pour le moment pas de temps à perdre sur les factory
+var callbackDataReceived = null;
+
+socket.on('initVote', function(data, label){
+
+	if(callbackDataReceived !== null){
+		callbackDataReceived(data, label);
+	}
+});
+
+socket.on('updateVote', function(data){
+
+	console.log("Recu updateVote");
+
+	if(callbackDataReceived !== null){
+		callbackDataReceived(data, null);
+	}
+});
+
+
 var tweb = angular.module('tweb', ['ngRoute', 'chart.js']);
 
 tweb.controller('mainController', ['$route', '$routeParams', '$location',
@@ -15,15 +40,21 @@ tweb.controller('headerController', ['$scope', '$location',
 
 	}]);
 
-tweb.controller('firstController', ['$scope', function($scope){
+tweb.controller('questionController', ['$scope', function($scope){
+	$scope.vote = function(id){
+  		socket.emit('voteFor', id);
+  	};
 
-	$scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-  	$scope.data = [300, 500, 100];
+  	$scope.reset = function(){
+  		console.log("RESET");
 
-  	$scope.home = true;
-  	$scope.admin = false;
+  		socket.emit('reset');
+  	};
+}]);
 
-  	$scope.title = "Vos résultats";
+tweb.controller('audienceController', ['$scope', function($scope){
+
+  	$scope.question = "Comment trouvez-vous ce site ?";
 
 }]);
 
@@ -31,21 +62,74 @@ tweb.controller('firstController', ['$scope', function($scope){
 tweb.config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.
-			when('/home', {
-				templateUrl: 'home.html',
-				controller: 'firstController'
+			when('/audience', {
+				templateUrl: 'audience.html',
+				controller: 'audienceController'
 			}).
-			when('/admin', {
-				templateUrl: 'admin.html',
-				controller: 'secondController'
+			when('/board', {
+				templateUrl: 'board.html',
+				controller: 'boardController'
+			}).
+			when('/debug', {
+				templateUrl: 'debug.html',
+				controller: 'debugController'
 			}).
 			otherwise({
-				templateUrl: 'home.html',
-				controller: 'firstController'
+				templateUrl: 'audience.html',
+				controller: 'audienceController'
 			});
 	}]);
 
-tweb.controller('secondController', function($scope){
-	$scope.home = false;
-  	$scope.admin = true;
+tweb.controller('boardController', function($scope){
+	$scope.title = "Vos résultats";
+
+	$scope.labels = [];
+  	$scope.data = [];
+
+  	callbackDataReceived = function(data, labels){
+
+  		console.log("callback called");
+
+  		$scope.data = data;
+
+  		if(labels !== null){
+  			$scope.labels = labels;
+  		}
+
+  		console.log($scope.labels);
+
+  		// Permet de dire que la vue a changer et qu'il faut la reconsidérer
+  		$scope.$apply();
+  	};
+
+  	// RequestInit est appelé car le serveur ne peut pas initialiser de lui-même dès la connexion du client car callbackDataReceived ne sera pas encore initialisé
+  	socket.emit('requestInit');
+});
+
+tweb.controller('debugController', function($scope){
+	$scope.title = "";
+
+	$scope.labels = [];
+  	$scope.data = [];
+  	//$scope.series = [];
+
+  	callbackDataReceived = function(data, labels){
+
+  		console.log("callback called");
+
+  		$scope.data = data;
+
+  		if(labels !== null){
+  			$scope.labels = labels;
+  		}
+
+  		console.log($scope.labels);
+
+  		// Permet de dire que la vue a changer et qu'il faut la reconsidérer
+  		$scope.$apply();
+  	};
+
+  	// RequestInit est appelé car le serveur ne peut pas initialiser de lui-même dès la connexion du client car callbackDataReceived ne sera pas encore initialisé
+  	socket.emit('requestInit');
+
 });
