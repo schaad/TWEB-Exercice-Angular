0a72dbcd5b45beb93b506de962318df2cf3e12da
Valentin Schaad <valentin.schaad@heig-vd.ch>
2015-11-19 12:46:17 +0100

    Résolution des tab actifs

--- public/js/app.js
+++ public/js/app.js
@@ -5,17 +5,26 @@ tweb.controller('mainController', ['$route', '$routeParams', '$location',
     this.$route = $route;
     this.$location = $location;
     this.$routeParams = $routeParams;
-}])
+}]);
+
+tweb.controller('headerController', ['$scope', '$location', 
+	function($scope, $location) {
+		$scope.isActive = function(route) {
+        	return route === $location.path();
+    	};
+
+	}]);
 
 tweb.controller('firstController', ['$scope', function($scope){
 
 	$scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
   	$scope.data = [300, 500, 100];
 
-  	$scope.home = "active";
-  	$scope.admin = "";
+  	$scope.home = true;
+  	$scope.admin = false;
 
   	$scope.title = "Vos résultats";
+
 }]);
 
 
@@ -37,6 +46,6 @@ tweb.config(['$routeProvider',
 	}]);
 
 tweb.controller('secondController', function($scope){
-	$scope.home = "";
-  	$scope.admin = "active";
+	$scope.home = false;
+  	$scope.admin = true;
 });
